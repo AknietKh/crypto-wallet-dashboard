7abe3903b9c42f6bcf36c75fc1f38311f057e401
Akniet_Kh <akniethakim@gmail.com>
2022-01-09 19:25:22 +0600

    upd: add token cards

--- components/App/TokenCard/index.vue
+++ components/App/TokenCard/index.vue
@@ -1,15 +1,85 @@
 <template>
   <div class="token-card">
-    <img :src="tokensLogo[]" alt="">
+    <div class="token-card__header token-card__header_mb">
+      <img
+        class="token-card__logo"
+        :src="tokenLogo"
+        :alt="token.symbol.toUpperCase()"
+      >
+      <p class="token-card__title">
+        {{ token.symbol.toUpperCase() }}
+      </p>
+    </div>
+    <div class="token-card__body">
+      <p class="token-card__amount">
+        {{ token.amount }} {{ token.symbol }}
+      </p>
+    </div>
   </div>
 </template>
 
-<script>
-export default {
+<script lang="ts">
+import { PropType } from 'vue'
+import { mapGetters } from 'vuex'
+import MainMixin from '~/mixins/MainMixin'
 
+export interface ITokenCard {
+  symbol: string,
+  amount: number,
 }
+
+export default MainMixin.extend({
+  name: 'TokenCard',
+  props: {
+    token: {
+      type: Object as PropType<ITokenCard>,
+      default: ():ITokenCard => ({
+        symbol: '',
+        amount: 0
+      })
+    }
+  },
+  computed: {
+    ...mapGetters({
+      tokensLogo: 'main/getTokensUrl'
+    }),
+    tokenLogo () {
+      return this.tokensLogo[this.token.symbol.toUpperCase()] || this.Require('default-token.svg')
+    }
+  }
+})
 </script>
 
-<style>
-
+<style lang="scss">
+  .token-card {
+    min-width: 206px;
+    padding: 20px;
+    background-color: $token-card-bg;
+    border-radius: 14px;
+    &__header {
+      display: grid;
+      grid-template-columns: 30px 1fr;
+      align-items: center;
+      grid-column-gap: 10px;
+      &_mb {
+        margin-bottom: 14px;
+      }
+    }
+    &__logo {
+      width: 30px;
+      height: 30px;
+      border-radius: 50%;
+    }
+    &__title {
+      font-size: 18px;
+      font-weight: 600;
+      color: $main-text-color;
+    }
+    &__amount {
+      font-size: 16px;
+      font-weight: normal;
+      text-align: right;
+      color: $secondary-text-color;
+    }
+  }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,121 @@
 <template>
   <div class="main-page">
-    <balance-card />
+    <section class="main-page__section">
+      <balance-card />
+    </section>
+    <div class="main-page__section">
+      <div class="tokens-section">
+        <div class="token-cards">
+          <token-card v-for="token in tokens" :key="token.symbol" :token="token" />
+        </div>
+        <button type="button" class="add-token">
+          <p class="add-token__text">
+            {{ $t('add-token.btn') }}
+          </p>
+          <span class="add-token__plus">+</span>
+        </button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import MainMixin from '~/mixins/MainMixin'
 import BalanceCard from '~/components/App/BalanceCard/index.vue'
+import TokenCard from '~/components/App/TokenCard/index.vue'
 
 export default MainMixin.extend({
   name: 'MainPage',
   components: {
-    BalanceCard
+    BalanceCard,
+    TokenCard
+  },
+  data () {
+    return {
+    }
+  },
+  computed: {
+    tokens () {
+      return [
+        {
+          symbol: 'ETH',
+          amount: 10
+        },
+        {
+          symbol: 'BTC',
+          amount: 100
+        },
+        {
+          symbol: 'ETH',
+          amount: 10
+        },
+        {
+          symbol: 'BTC',
+          amount: 100
+        },
+        {
+          symbol: 'ETH',
+          amount: 10
+        },
+        {
+          symbol: 'BTC',
+          amount: 100
+        },
+        {
+          symbol: 'ETH',
+          amount: 10
+        },
+        {
+          symbol: 'BTC',
+          amount: 100
+        }
+      ]
+    }
   }
 })
 </script>
 
 <style lang="scss" scoped>
   .main-page {
-
+    &__section {
+      margin-bottom: 25px;
+    }
+  }
+  .tokens-section {
+    display: grid;
+    grid-template-columns: 1fr auto;
+    grid-column-gap: 20px;
+  }
+  .token-cards {
+    @include base-scrollbar;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    overflow-x: auto;
+    border-top-right-radius: 14px;
+    border-bottom-right-radius: 14px;
+    // box-shadow: 0px 10px 40px rgba(3, 3, 3, 0.1);
+    :not(:last-child) {
+      margin-right: 20px;
+    }
+  }
+  .add-token {
+    min-width: 206px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    grid-column-gap: 10px;
+    padding: 20px;
+    background-color: $default-background;
+    border: 2px solid $header-border;
+    border-radius: 14px;
+    &__text {
+      font-size: 16px;
+      font-weight: 600;
+      color: $main-text-color;
+    }
+    &__plus {
+      font-size: 26px;
+    }
   }
 </style>
